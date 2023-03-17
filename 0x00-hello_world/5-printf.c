f9026197e5f3a771b1144b2e22046817fbc26e77
henry offor <henryoffor007@gmail.com>
2023-03-16 21:34:33 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+*main - the entry point for all functions
+*
+*Return: function output
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
