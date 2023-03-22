d16f773c1b91fab9a1f498e5097ba356c352df66
henry offor <henryoffor007@gmail.com>
2023-03-22 10:24:51 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _islower - shows 1 if the input is lower case and other cases if
+ * 0
+ *
+ * Return: 1 for lower case. 0 for ther cases.
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
+
