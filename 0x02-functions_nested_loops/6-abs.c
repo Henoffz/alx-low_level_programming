394fb3f29fe0c3ddf46038435444ff07b8c82ed6
henry offor <henryoffor007@gmail.com>
2023-03-22 12:19:26 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - calculate the absolute value of an integer.
+ *
+ * @i:enter number as integer.
+ *
+ * Return: absolute value
+ */
+
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
