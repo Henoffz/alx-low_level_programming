4107f7ea12b0a155403e4c8963fc65b220230f1a
henry offor <henryoffor007@gmail.com>
2023-04-20 19:16:47 -0700

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints the opcodes of its own main function
+ *
+ * @argc: number of arguments
+ * @argv: arrays of values
+ *
+ * Return: 0 upon success
+ */
+int main(int argc, char **argv)
+{
+	int i, n;
+	char *ptr;
+
+	if (argc != 2)
+	{
+		printf("Exit\n");
+		exit(1);
+	}
+	bytess = atoi(argv[1]);
+	if n < 0
+	{
+		printf("Exit\n");
+		exit(2);
+	}
+	ptr = (char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == n - 1)
+		{
+			printf("%02hhx ", ptr[i]);
+			break;
+		}
+		printf("%02hhx ", ptr[i]);
+		return (0);
+	}
+}
