c81d58ab3d2638b8220075df5a29e99ccf3d1361
henry offor <henryoffor007@gmail.com>
2023-05-04 17:44:29 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -17,9 +17,9 @@ int get_bit(unsigned long int n, unsigned int index)
 
 	adj = adj << index;
 
-	if (n & adj)
+	if ((n & adj) == 0)
 	{
-		return (1);
+		return (0);
 	}
-	return (0);
+	return (1);
 }
